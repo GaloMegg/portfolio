283ecdb5160523807d8dc388a44d373838e0acea
WtsCrackin <galomeggiolarobul@gmail.com>
2021-12-19 11:00:48 -0300

    responsive adaptation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
+$("#home").on("click", ScrollDownHome)
+function ScrollDownHome(e) {
+    e.preventDefault()
+    $("html , body").animate({
+        scrollTop: $("#homeScroll").offset().top
+    }, 1000)
+}
 
-$("#aboutMe").on("click", ScrollDown)
-
-function ScrollDown(e) {
+$("#aboutMe").on("click", ScrollDownAboutMe)
+function ScrollDownAboutMe(e) {
     e.preventDefault()
     $("html , body").animate({
         scrollTop: $("#aboutMeScroll").offset().top
+    }, 1000)
+}
+
+$("#proyects").on("click", ScrollDownProyects)
+function ScrollDownProyects(e) {
+    e.preventDefault()
+    $("html , body").animate({
+        scrollTop: $("#proyectsScroll").offset().top
+    }, 1000)
+}
+
+$("#contact").on("click", ScrollDownContact)
+function ScrollDownContact(e) {
+    e.preventDefault()
+    $("html , body").animate({
+        scrollTop: $("#contactScroll").offset().top
     }, 1000)
 }
