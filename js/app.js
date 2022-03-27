8d1acb628bf794ff0c9732d3ce13bc8b7279dc9d
WtsCrackin <galomeggiolarobul@gmail.com>
2022-03-27 19:31:49 -0300

    cursor changes

--- js/app.js
+++ js/app.js
@@ -30,3 +30,32 @@ function ScrollDownContact(e) {
         scrollTop: $("#contactScroll").offset().top
     }, 1000)
 }
+
+window.addEventListener("mousemove", function (e) {
+    $(".cursor").css({
+        left: (e.clientX - 8),
+        top: (e.clientY - 8)
+    })
+})
+
+document.addEventListener("mousedown", function (e) {
+    $(".cursor__element").addClass("cursor__clicked")
+})
+document.addEventListener("mouseup", function (e) {
+    $(".cursor__element").removeClass("cursor__clicked")
+})
+document.addEventListener("mouseenter", function (e) {
+    $(".cursor").removeClass("hidden")
+})
+document.addEventListener("mouseleave", function (e) {
+    $(".cursor").addClass("hidden")
+})
+
+document.querySelectorAll("a, button").forEach((e) => {
+    e.addEventListener("mouseover", () => {
+        $(`.cursor__element`).addClass( "hoverlinked")
+    })
+    e.addEventListener("mouseout", () => {
+        $(`.cursor__element`).removeClass( "hoverlinked")
+    })
+})
